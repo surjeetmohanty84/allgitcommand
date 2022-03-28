02fc904e13bfeec7ea1f585cfad7c29245386daa
surjeetmohanty84 <98092313+surjeetmohanty84@users.noreply.github.com>
2022-03-29 00:57:57 +0530

    One new method added in master branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 
 public class HelloWorld {
-
+	public void masterM2(){
+		System.out.println("Master method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
