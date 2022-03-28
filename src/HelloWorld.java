69be740edb68e171869c092bd30b09f246c14a52
surjeetmohanty84 <98092313+surjeetmohanty84@users.noreply.github.com>
2022-03-29 01:04:36 +0530

    Second method added in master branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 	public void masterM2(){
 		System.out.println("Master method");
 	}
+	public void masterM3(){
+		System.out.println("Master method 3.........");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
